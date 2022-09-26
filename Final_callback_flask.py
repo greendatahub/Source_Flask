4243f1438fe07f63d9c6280ec75f0606cc43cff8
greendatahub <113531517+greendatahub@users.noreply.github.com>
2022-09-26 21:11:20 +0900

    Update Final_callback_flask.py

--- Final_callback_flask.py
+++ Final_callback_flask.py
@@ -129,8 +129,9 @@ def preidct():
         length = len(final_DF)-2
         model = prediction(model_path,scaler_path)
         response = model.prediction_output(final_DF,length,size,return_date)
+        return json.dumps(response)
         #return make_response(jsonify(response),200)
-        return render_template('index.html', response = make_response(jsonifyresponse))
+        #return render_template('index.html', response = make_response(jsonifyresponse))
     
 
 
