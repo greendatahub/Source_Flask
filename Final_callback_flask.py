5778707a169cfa3d32e78f4f2b747fcfc223a694
greendatahub <113531517+greendatahub@users.noreply.github.com>
2022-09-26 20:03:15 +0900

    Update Final_callback_flask.py

--- Final_callback_flask.py
+++ Final_callback_flask.py
@@ -110,18 +110,13 @@ class prediction(object):
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-'''
-@app.route('/', methods = ['POST','GET'])
-def index():
-        return render_template('index.html')
-'''
-
-
 
 @app.route('/', methods = ['POST','GET'])
 def index():
-    if request.method == "GET":
         return render_template('index.html')
+
+@app.route('/', methods = ['POST','GET'])
+def predict():
     if request.method == "POST":
         path1 = request.form['upload-file']
         path2 = '/home/ubuntu/Source_flask/Past_Data.xlsx'
