aa0ce3b1991539bc034d0892a009a66fc36daf05
greendatahub <113531517+greendatahub@users.noreply.github.com>
2022-09-26 13:06:14 +0900

    Update Final_callback_flask.py

--- Final_callback_flask.py
+++ Final_callback_flask.py
@@ -112,7 +112,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/predict', methods = ['POST','GET'])
+@app.route('/', methods = ['POST','GET'])
 def predict():
     if request.method == "POST":
         path1 = request.form['upload-file']
