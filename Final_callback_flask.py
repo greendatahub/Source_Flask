92dfcbf6f2d4646a2540423dd8ae442257fad732
greendatahub <113531517+greendatahub@users.noreply.github.com>
2022-09-27 02:21:53 +0900

    Update Final_callback_flask.py

--- Final_callback_flask.py
+++ Final_callback_flask.py
@@ -119,19 +119,18 @@ def index():
 @app.route('/', methods = ['POST','GET'])
 def predict():
     if request.method == "POST":
-        return "Hello World!"
-        #path1 = request.form['upload-file']
-        #path2 = '/home/ubuntu/Source_flask/Past_Data.xlsx'
-        #model_path = '/home/ubuntu/Source_flask/Final_LSTM.hdf5'
-        #scaler_path = '/home/ubuntu/Source_flask/scaler.joblib'
-        #return_date = request.form['return_date']
-        #previous_data, start_date,size = preprocessing_ML(path1,return_date)
-        #now_data = preprocessing_ML2(path2,start_date)
-        #final_DF = pd.concat([previous_data,now_data])
-        #length = len(final_DF)-2
-        #model = prediction(model_path,scaler_path)
-        #response = model.prediction_output(final_DF,length,size,return_date)
-        #return make_response(jsonify(response),200)
+        path1 = request.form['upload-file']
+        path2 = '/home/ubuntu/Source_flask/Past_Data.xlsx'
+        model_path = '/home/ubuntu/Source_flask/Final_LSTM.hdf5'
+        scaler_path = '/home/ubuntu/Source_flask/scaler.joblib'
+        return_date = request.form['return_date']
+        previous_data, start_date,size = preprocessing_ML(path1,return_date)
+        now_data = preprocessing_ML2(path2,start_date)
+        final_DF = pd.concat([previous_data,now_data])
+        length = len(final_DF)-2
+        model = prediction(model_path,scaler_path)
+        response = model.prediction_output(final_DF,length,size,return_date)
+        return make_response(jsonify(response),200)
         #return response
         #return render_template('index.html', response = make_response(jsonify(response)))
     
