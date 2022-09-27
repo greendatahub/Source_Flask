4b79da916660e3362f2e42724a78b495bf153eed
greendatahub <113531517+greendatahub@users.noreply.github.com>
2022-09-27 18:17:44 +0900

    Update Final_callback_flask.py

--- Final_callback_flask.py
+++ Final_callback_flask.py
@@ -134,10 +134,13 @@ def predict():
         length = len(final_DF)-2
         model = prediction(model_path,scaler_path)
         response = model.prediction_output(final_DF,length,size,return_date)
+        return response
+        '''
         if not response:
             print("server err! 500")
             return make_response(jsonify(response),500)
         return make_response(jsonify(response),200)
+        '''
         #return response
         #return make_response(jsonify(response),200)
         #return json.dumps(response)
