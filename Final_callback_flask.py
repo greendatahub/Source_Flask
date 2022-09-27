6f189e74cbd0583c218b84460bdf9a274889430c
greendatahub <113531517+greendatahub@users.noreply.github.com>
2022-09-27 16:18:29 +0900

    Update Final_callback_flask.py

--- Final_callback_flask.py
+++ Final_callback_flask.py
@@ -118,7 +118,7 @@ def index():
         return render_template('index.html')
     if response:
         #return response
-        return render_template('index.html', response = make_response(jsonify(response)))
+        return make_response(jsonify(response)))
 @app.route('/predict', methods = ['GET','POST'])
 def predict():
     global response
